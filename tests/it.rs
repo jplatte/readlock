38b7636c5ec42dc858fa6cc0784dc334ab00af46
Jonas Platte <jplatte+git@posteo.de>
2023-02-19 17:48:07 +0100

    Add a test
    It passes miri!

---
+++ tests/it.rs
@@ -0,0 +1,19 @@
+use std::{self, thread, time::Duration};
+
+use readlock::Shared;
+
+#[test]
+fn parallel_read_write() {
+    let mut shared = Shared::new(1);
+    let readlock = Shared::get_read_lock(&shared);
+
+    let join_handle = thread::spawn(move || while *readlock.lock() < 1024 {});
+    thread::sleep(Duration::from_millis(5));
+    for _ in 0..10 {
+        let value: i32 = *shared;
+        *Shared::lock(&mut shared) += value;
+    }
+
+    thread::sleep(Duration::from_millis(5));
+    assert!(join_handle.is_finished());
+}
